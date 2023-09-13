cac3973c0818ab3d882d7721419a4dc96eb9b492
kimmyungho <kimmyungho1003@naver.com>
2023-09-14 06:35:03 +0900

    파일 복구

---
+++ src/main/java/com/example/spring_advanced/app/v3/OrderServiceV3.java
@@ -0,0 +1,25 @@
+package com.example.spring_advanced.app.v3;
+
+import com.example.spring_advanced.trace.TraceStatus;
+import com.example.spring_advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV3 {
+    private final OrderRepositoryV3 orderRepository;
+    private final LogTrace trace;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.begin()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
